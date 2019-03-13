8fea81165006ff4699029f7a1ff9e82420802075
APodolskiy <originalea@yandex.ru>
2019-03-13 10:06:18 +0300

    Add summary writing to tensorboard

--- tpr_rnn/train.py
+++ tpr_rnn/train.py
@@ -2,10 +2,12 @@ from argparse import ArgumentParser
 import logging
 import json
 from pathlib import Path
+import shutil
 from tqdm import tqdm
-from typing import Dict, Optional
+from typing import Dict
 
 import numpy as np
+from tensorboardX import SummaryWriter
 
 import torch
 import torch.optim as optim
@@ -19,10 +21,20 @@ from tpr_rnn.model.utils import WarmupScheduler
 logger = logging.getLogger(__name__)
 
 
-def train(config: Dict,
-          serialization_path: Optional[str] = None,
+def train(config: Dict[str, Dict],
+          serialization_path: str,
           eval_test: bool = False,
           force: bool = True) -> None:
+    # Create serialization dir
+    dir_path = Path(serialization_path)
+    if dir_path.exists() and force:
+        shutil.rmtree(dir_path)
+    dir_path.mkdir(parents=True, exist_ok=False)
+    model_path = dir_path / "model.pt"
+    config_path = dir_path / "config.json"
+    writer = SummaryWriter(log_dir=str(dir_path))
+
+    # Read config
     data_config = config["data"]
     trainer_config = config["trainer"]
     model_config = config["model"]
@@ -53,10 +65,6 @@ def train(config: Dict,
     train_data_loader = DataLoader(train_dataset, batch_size=trainer_config["batch_size"], shuffle=True)
     valid_data_loader = DataLoader(valid_dataset, batch_size=valid_batch_size)
 
-    if eval_test:
-        test_dataset = TensorDataset(*[torch.LongTensor(a) for a in test_raw_data[:-1]])
-        test_data_loader = DataLoader(test_dataset, batch_size=test_batch_size)
-
     # Create model
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     model = TprRnn(model_config).to(device)
@@ -74,7 +82,7 @@ def train(config: Dict,
     for i in range(trainer_config["epochs"]):
         logging.info(f"##### EPOCH: {i} #####")
         scheduler.step()
-
+        # Train
         model.train()
         correct = 0
         train_loss = 0
@@ -93,6 +101,8 @@ def train(config: Dict,
             optimizer.step()
         train_acc = correct / train_epoch_size
         train_loss = train_loss / train_epoch_size
+
+        # Validation
         model.eval()
         correct = 0
         valid_loss = 0
@@ -106,12 +116,23 @@ def train(config: Dict,
                 valid_loss += loss.sum().item()
             valid_acc = correct / valid_epoch_size
             valid_loss = valid_loss / valid_epoch_size
+
+        writer.add_scalars("accuracy", {"train": train_acc,
+                                        "validation": valid_acc}, i)
+        writer.add_scalars("loss", {"train": train_loss,
+                                    "validation": valid_loss}, i)
+
         logging.info(f"\nTrain accuracy: {train_acc:.3f}, loss: {valid_loss:.3f}"
                      f"\nValid accuracy: {valid_acc:.3f}, loss: {train_loss:.3f}")
         if optimizer_config.get("decay", False) and valid_loss < optimizer_config["decay_thr"] and not decay_done:
             scheduler.decay_lr(optimizer_config["decay_factor"])
             decay_done = True
+
     if eval_test:
+        # Evaluation on test data
+        test_dataset = TensorDataset(*[torch.LongTensor(a) for a in test_raw_data[:-1]])
+        test_data_loader = DataLoader(test_dataset, batch_size=test_batch_size)
+
         model.eval()
         correct = 0
         test_loss = 0
@@ -127,21 +148,17 @@ def train(config: Dict,
             test_loss = test_loss / test_epoch_size
         logging.info(f"Test accuracy: {test_acc:.3f}, loss: {test_loss:.3f}")
 
-    if serialization_path is not None:
-        dir_path = Path(serialization_path)
-        dir_path.mkdir(parents=True, exist_ok=force)
-        model_path = dir_path / "model.pt"
-        config_path = dir_path / "config.json"
-        torch.save(model.state_dict(), model_path.absolute())
-        with config_path.open("w") as fp:
-            json.dump(config, fp)
+    torch.save(model.state_dict(), model_path.absolute())
+    with config_path.open("w") as fp:
+        json.dump(config, fp, indent=4)
+    writer.close()
 
 
 if __name__ == "__main__":
     parser = ArgumentParser(description="Training script for TPR RNN.")
     parser.add_argument("--config-file", type=str, metavar='PATH', required=True,
                         help="Path to the model config file")
-    parser.add_argument("--serialization-path", type=str, metavar='PATH', required=False,
+    parser.add_argument("--serialization-path", type=str, metavar='PATH', required=True,
                         help="Serialization directory path")
     parser.add_argument("--eval-test", default=False, action='store_true',
                         help="Whether to eval model on test dataset after training (default: False)")
